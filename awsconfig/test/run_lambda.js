cd055c509642d4fcc9bab2fdcfaf3caeb17399f9
Alex Ough <alex.ough@gmail.com>
2015-09-23 15:52:27 -0500

    removed external ids

--- awsconfig/test/run_lambda.js
+++ awsconfig/test/run_lambda.js
@@ -10,10 +10,8 @@ if (!module || (module != 'checker' && module != 'enabler' && module != 'remover
 
 var event = {
   "federateAccount": "089476987273",
-  "__account": "876224653878",
-  "__roleExternalId": "4e709cfd-e41b-4425-84cb-64461f23465c",
   "account": "089476987273",
-  "roleExternalId": "88df904d-c597-40ef-8b29-b767aba1eaa4",
+  "roleExternalId": "",
   "roleName": "sgas_dev_admin",
   "sessionName": "abcde",
   "region": "us-east-1"
