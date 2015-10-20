b09c6b3b40b956a4f3e2f18f3954143952a06b33
Alex Ough <alex.ough@gmail.com>
2015-10-20 09:50:59 -0500

    added more build targets and moved billingalert populator creation to initial setup

--- billingalert/build.f/particles-populator/particles/helpers/propertyValue.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(str,options) {
-  if ((str != null) && (typeof str != 'object') && (str.indexOf('{') != 0)) {
-    return '"'+str+'"';
-  } else {
-    return str;
-  }
-};
