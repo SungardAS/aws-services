37340fdb3c18d30c6a2c3852617c0b726b8014b4
Alex Ough <alex.ough@gmail.com>
2015-11-03 16:32:43 -0600

    fixed not to add duplicated metric data

--- billingalert/metrics.js
+++ billingalert/metrics.js
@@ -61,6 +61,29 @@ function Metrics() {
    Unit: 'None'
   };
 
+  // metrics for IncreasedPercentages Query
+  this.IncreasedPercentagesMetricQuery = {
+    StartTime: null,
+    EndTime: null,
+    MetricName: 'IncreasedPercentages',
+    Namespace: 'CTOBilling',
+    Period: 60,
+    Statistics: [
+     'SampleCount', 'Average', 'Sum', 'Minimum', 'Maximum'
+    ],
+    Dimensions: [
+      {
+        Name: 'LinkedAccount',
+        Value: null
+      },
+      {
+        Name: 'None',
+        Value: 'Percent'
+      }
+   ],
+   Unit: 'Percent'
+  };
+
   // metrics for IncreasedPercentages
   this.CTOIncreasedPercentagesMetricData = {
     MetricData: [
@@ -119,18 +142,50 @@ function Metrics() {
     aws_watch_remote.findMetricsStatistics(me.remoteInput);
   }
 
+  function buildIncreasedPercentagesMetricQuery() {
+    console.log('<<<Starting buildIncreasedPercentagesMetricQuery...');
+    var current = new Date();
+    var startTime = new Date();
+    //current.setMinutes(current.getMinutes() - 5);
+    startTime.setHours(startTime.getHours() - 24);
+    me.IncreasedPercentagesMetricQuery.StartTime = startTime;
+    me.IncreasedPercentagesMetricQuery.EndTime = current;
+    me.IncreasedPercentagesMetricQuery.Dimensions[0].Value = me.accountId;
+    me.localInput.metricQuery = me.IncreasedPercentagesMetricQuery;
+    console.log(JSON.stringify(me.localInput));
+    console.log('>>>...completed buildIncreasedPercentagesMetricQuery');
+    aws_watch_local.findMetricsStatistics(me.localInput);
+  }
+
   function buildIncreasedPercentagesMetricsData() {
+
     console.log('<<<Starting buildIncreasedPercentagesMetricsData...');
     console.log(JSON.stringify(me.remoteInput));
     var metrics = me.remoteInput.metrics.sort(function(a, b){return b.Timestamp - a.Timestamp}).splice(0,2);
     console.log(JSON.stringify(metrics));
+
+    // check if the new metric data has been generated in remoteRegion
+    var percentageMetrics = me.localInput.metrics;
+    if (me.localInput.metrics && me.localInput.metrics.length >= 2) {
+      percentageMetrics = me.localInput.metrics.sort(function(a, b){return b.Timestamp - a.Timestamp}).splice(0,2);
+    }
+    console.log(JSON.stringify(percentageMetrics));
+    if (percentageMetrics && percentageMetrics[0] && metrics[0]) {
+      console.log(percentageMetrics[0].Timestamp);
+      console.log(metrics[0].Timestamp);
+      if (percentageMetrics[0].Timestamp.getTime() == metrics[0].Timestamp.getTime()) {
+        console.log("no new EstimatedChargeds metric data, so just return");
+        me.callback(null, true);
+        return;
+      }
+    }
     var percentage = 0;
     if (metrics.length >= 2 && metrics[1].Maximum > 0) {
       percentage = ((metrics[0].Maximum - metrics[1].Maximum) / metrics[1].Maximum) * 100;
     }
     console.log(percentage);
     metricData = me.CTOIncreasedPercentagesMetricData;
-    metricData.MetricData[0].Timestamp = new Date();
+    metricData.MetricData[0].Timestamp = metrics[0].Timestamp;
     metricData.MetricData[0].Value = percentage;
     metricData.MetricData[0].Dimensions[0].Value = me.accountId;
     me.localInput.metricData = metricData;
@@ -153,7 +208,9 @@ function Metrics() {
 
     var flows = [
       {func:buildEstimatedChargesMetricsData, success:aws_watch_remote.findMetricsStatistics, failure:failed, error:errored},
-      {func:aws_watch_remote.findMetricsStatistics, success:buildIncreasedPercentagesMetricsData, failure:failed, error:errored},
+      {func:aws_watch_remote.findMetricsStatistics, success:buildIncreasedPercentagesMetricQuery, failure:failed, error:errored},
+      {func:buildIncreasedPercentagesMetricQuery, success:aws_watch_local.findMetricsStatistics, failure:failed, error:errored},
+      {func:aws_watch_local.findMetricsStatistics, success:buildIncreasedPercentagesMetricsData, failure:buildIncreasedPercentagesMetricsData, error:errored},
       {func:buildIncreasedPercentagesMetricsData, success:aws_watch_local.addMetricData, failure:failed, error:errored},
       {func:aws_watch_local.addMetricData, success:succeeded, failure:failed, error:errored},
     ]
