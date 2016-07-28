ac68b70048d247aad83084e72569416b9aeb4618
sonal_ojha <sonal.ojha@sungardas.com>
2016-07-28 20:08:21 +0530

    Removed a syntax error

--- awsconfigrules/index_security_group_checker.js
+++ awsconfigrules/index_security_group_checker.js
@@ -30,7 +30,7 @@ exports.handler = function (event, context) {
     else ruleParameters = {"vpcId": event.vpcId, "region": event.region, "groupName": event.groupName};
 
     if (invokingEvent) invokingEvent = JSON.parse(invokingEvent);
-    else invokingEvent = {"configurationItem": {"resourceType": "EC2 VPC", "resourceId": vpc_id, "configurationItemCaptureTime": new Date()}};
+    else invokingEvent = {"configurationItem": {"resourceType": "EC2 VPC", "resourceId": event.vpcId, "configurationItemCaptureTime": new Date()}};
 
     if(event.resultToken) var resultToken = event.resultToken;
     else var resultToken = "110ec58a-a0f2-4ac4-8393-c866d813b8d1";
@@ -54,7 +54,7 @@ exports.handler = function (event, context) {
 
     var flows = [
         {func:aws_sts.assumeRoles, success:aws_ec2.securityGroupHasRules, failure:failed, error:errored},
-        {func:aws_ec2.securityGroupHasRules, success:aws_config.sendEvaluation, failure:failed, error:errored},
+        {func:aws_ec2.securityGroupHasRules, success:aws_config.sendEvaluation, failure:aws_config.sendEvaluation, error:errored},
         {func:aws_config.sendEvaluation, success:succeeded, failure:failed, error:errored},
     ];
     aws_ec2.flows = flows;
