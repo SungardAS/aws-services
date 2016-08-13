b999304959fdc90a6c17e474a313137b0f6c465d
Alex Ough <alex.ough@sungardas.com>
2016-08-12 23:51:45 -0500

    cleaned up

--- ec2/metrics.js
+++ ec2/metrics.js
@@ -2,8 +2,8 @@
 // aws cloudwatch get-metric-statistics --metric-name CPUUtilization --start-time 2016-08-01T00:00:00 --end-time 2016-08-11T15:00:00 --period 1440 --namespace AWS/EC2 --statistics Minimum --dimensions Name=InstanceId,Value=<your-instance-id>
 
 var AWS = require('aws-sdk');
-
 var region = 'us-east-1';
+
 var ec2 = new AWS.EC2({region: region});
 var params = {
   /*DryRun: true || false,
@@ -27,53 +27,77 @@ var params = {
 var promise = ec2.describeInstances(params).promise();
 promise.then(function(data) {
   //console.log(JSON.stringify(data));
+  var instances = [];
+  var promises = [];
   data.Reservations.forEach(function(reservation) {
     reservation.Instances.forEach(function(instance) {
-      console.log(JSON.stringify(instance));
-      console.log("\n");
+      //console.log(instance);
+      //console.log("\n");
+      if (instance.State.Name == "running") {
+        instances.push(instance);
+        promises.push(getAWSCPUUtilizationMetricStatisticsByEC2Instance(instance.InstanceId));
+      }
     });
   });
-}).catch(function(err) {
-  console.log(err);
-});
-
-
-var AWS = require('aws-sdk');
-var region = 'us-east-1';
-
-var instanceId = '';
-var current = new Date();
-var cloudWatch = new AWS.CloudWatch({region: region});
-var startTime = new Date(current.getFullYear(), current.getMonth(), current.getDate());
-startTime.setHours(startTime.getHours() - 24*14);
-var endTime = current;
-var AWSCPUUtilizationMetricQuery = {
-  StartTime: startTime,
-  EndTime: endTime,
-  MetricName: 'CPUUtilization',
-  Namespace: 'AWS/EC2',
-  Period: 60 * 60 * 4,
-  Statistics: [
-   'SampleCount', 'Average', 'Sum', 'Minimum', 'Maximum'
-  ],
-  Dimensions: [
-    {
-      Name: 'InstanceId',
-      Value: instanceId
-    }
- ],
- Unit: 'Percent'
-};
-var params = AWSCPUUtilizationMetricQuery;
-var promise = cloudWatch.getMetricStatistics(params).promise();
-promise.then(function(data) {
-  //console.log(data);
-  //console.log(JSON.stringify(data));
-  metrics = data.Datapoints.sort(function(a, b){return b.Timestamp - a.Timestamp});
-  metrics.forEach(function(metric) {
-    console.log(JSON.stringify(metric));
+  return [instances, promises];
+}).then(function(instances_promises) {
+  var instances = instances_promises[0];
+  var promises = instances_promises[1];
+  return Promise.all(promises).then(function(dataArray) {
+    for(var i = 0; i < dataArray.length; i++) {
+      metrics = dataArray[i].Datapoints.sort(function(a, b){return b.Timestamp - a.Timestamp});
+      //console.log(instances[i].InstanceId)
+      //console.log(metrics);
+      //console.log("\n");
+      instances[i].Metrics = metrics;
+    };
+    return instances;
+  });
+}).then(function(instances) {
+  instances.forEach(function(instance) {
+    console.log(instance);
     console.log("\n");
   });
 }).catch(function(err) {
   console.log(err);
 });
+
+
+function getAWSCPUUtilizationMetricStatisticsByEC2Instance(instanceId) {
+
+  var cloudWatch = new AWS.CloudWatch({region: region});
+  var startTime = new Date();
+  //startTime.setHours(startTime.getHours() - 24*14);
+  startTime.setHours(startTime.getHours() - 24);
+  var endTime = new Date();
+  var AWSCPUUtilizationMetricQuery = {
+    StartTime: startTime,
+    EndTime: endTime,
+    MetricName: 'CPUUtilization',
+    Namespace: 'AWS/EC2',
+    Period: 60 * 60 * 4,
+    Statistics: [
+     'SampleCount', 'Average', 'Sum', 'Minimum', 'Maximum'
+    ],
+    Dimensions: [
+      {
+        Name: 'InstanceId',
+        Value: instanceId
+      }
+   ],
+   Unit: 'Percent'
+  };
+  var promise = cloudWatch.getMetricStatistics(AWSCPUUtilizationMetricQuery).promise();
+  /*promise.then(function(data) {
+    //console.log(data);
+    //console.log(JSON.stringify(data));
+    metrics = data.Datapoints.sort(function(a, b){return b.Timestamp - a.Timestamp});
+    metrics.forEach(function(metric) {
+      console.log(JSON.stringify(metric));
+      console.log("\n");
+    });
+  }).catch(function(err) {
+    console.log(err);
+  });*/
+  return promise;
+}
