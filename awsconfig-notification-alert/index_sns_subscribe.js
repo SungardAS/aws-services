f67a762a7c0f7a0caf1f143f2b10c019540f9ac4
bhargavapratik <pratik.bhargava@sungardas.com>
2016-08-31 19:32:28 +0530

    MA-15: Modified code for Sns-Lambda Subscrition

--- awsconfig-notification-alert/index_sns_subscribe.js
+++ awsconfig-notification-alert/index_sns_subscribe.js
@@ -1,9 +1,9 @@
-
 exports.handler = function (event, context) {
-
-  var aws_topic = new (require('../lib/aws/topic.js'))();
-  var aws_lambda = new (require('../lib/aws/lambda.js'))(); 
-  var aws_sts = new (require('../lib/aws/sts'))();
+var provider = new (require('../lib/aws/assume_role_provider'))();
+var aws_topic = new (require('../lib/aws/topic.js'))();
+var aws_lambda = new (require('../lib/aws/lambda.js'))(); 
+var aws_sts = new (require('../lib/aws/sts.js'))();
+var AWS = require('aws-sdk');
 
 if (!event.federateRoleName)  event.federateRoleName = "federate";
 
@@ -22,56 +22,81 @@ if (!event.federateRoleName)  event.federateRoleName = "federate";
   if (sessionName == null || sessionName == "") {
     sessionName = "session";
   }
+    
   var fs = require("fs");
   data = fs.readFileSync(__dirname + '/json/data.json', {encoding:'utf8'});
   data_json = JSON.parse(data);
-
-var topicArn = "arn:aws:sns:" + event.region + ":" + event.account + ":" + data_json.topicName;
-var functionArn = "arn:aws:lambda:" + event.region + ":" + event.federateAccount + ":function:" + data_json.saverFuncName;
-//var action = [];
- //action.push({data_json.actionName[0]});
-//console.log(action);
+  
+  
 var input = {
     sessionName: sessionName,
     roles: roles,
     region: event.region,
-    topicName : data_json.topicName,
+    //topicName : data_json.topicName,
+    topicName: event.topicName,
     functionName : data_json.saverFuncName,
-  //  principal: event.principal,
 //    sourceArn:  data_json.sourceArn,
-//    sourceAccount: data_json.sourceAccount,
-      sourceAccount: event.sourceAccount,
 //    AccountId: data_json.AccountId,
-    actionName: event.action,
-//    actionName: data_json.actionName,
+    AccountId: event.AccountId,
     statementId: event.statementId, //unique string, some uuid from api
     topicArn : null,
-    functionArn : functionArn,
-    sourceArn : topicArn
+    functionArn: null,
+    sourceArn : null,
+    creds : null
   };
 
+   
+   var me = this;
+   
+   function getAuth(input)
+   {
+	   console.log(input);
+
+	provider.getCredential(roles,sessionName,0,null, function(err, data) {
+      if(err) {
+        console.log("failed to get credential : " + err);
+        callback(err);
+      }
+      else {
+ //       console.log(data);
+        input.creds = data;
+    	console.log(">>>>>>>>>>>>>> Exiting from getAuth")
+		aws_topic.findTopic(input);
+      }
+    }); 
+   }
+   
+   function findLambdaFunction(input)
+   {
+     console.log('<<<Starting find Lambda function...');	  
+	  aws_lambda.findFunction(input);
+	   input.sourceArn = input.topicArn;
+   }
+  function addTopicPermisson(input) {
+	console.log('<<<Starting add sns topic Permission function...');
+    aws_topic.addPermission(input);
+    input.creds = null;
+  }
+  
+
   function succeeded(input) { context.done(null, true); }
-  function failed(input) { context.done(null, false); }
+  function failed(input) { console.log(">>>>>  fail >>>>>");context.done(null, false); }
   function errored(err) { context.fail(err, null); }
 
-  var flows = [
-    {func:aws_lambda.addPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
-    {func:aws_sts.assumeRoles, success:aws_topic.findTopic, failure:failed, error:errored},
-    {func:aws_topic.findTopic, success:aws_topic.addPermission, failure:aws_topic.createTopic, error:errored},
-    {func:aws_topic.createTopic, success:aws_topic.addPermission, failure:failed, error:errored},
-    {func:aws_topic.addPermission, success:aws_topic.subscribeLambda, failure:failed, error:errored},
-    {func:aws_topic.subscribeLambda, success:succeeded, failure:failed, error:errored},
-  
- //{func:aws_lambda.findFunction, success:aws_lambda.addPermission, failure:failed, error:errored},
-   // {func:aws_lambda.addPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
-    //{func:aws_sts.assumeRoles, success:aws_topic.findTopic, failure:failed, error:errored},
-   // {func:aws_topic.findTopic, success:aws_topic.subscribeLambda, failure:aws_topic.createTopic, error:errored},
-   // {func:aws_topic.createTopic, success:aws_topic.subscribeLambda, failure:failed, error:errored},
-  //  {func:aws_topic.subscribeLambda, success:succeeded, failure:failed, error:errored},
-  ];
-  aws_topic.flows = flows;
-  aws_lambda.flows = flows;
-  aws_sts.flows = flows;
-
-  flows[0].func(input);
+    var flows = [
+        {func:getAuth, success:aws_topic.findTopic, failure:failed, error:errored},
+	{func:aws_topic.findTopic, success:addTopicPermisson, failure:aws_topic.createTopic, error:errored},
+        {func:aws_topic.createTopic, success:addTopicPermisson, failure:failed, error:errored},
+        {func:addTopicPermisson, success:aws_topic.addPermission, failure:failed, error:errored},
+	{func:aws_topic.addPermission, success:findLambdaFunction, failure:failed, error:errored},
+	{func:findLambdaFunction, success:aws_lambda.findFunction, failure:failed, error:errored},
+	{func:aws_lambda.findFunction, success:aws_lambda.addPermission, failure:failed, error:errored},
+	{func:aws_lambda.addPermission, success:aws_topic.subscribeLambda, failure:failed, error:errored},
+	{func:aws_topic.subscribeLambda, success:succeeded, failure:failed, error:errored},
+    ]
+    
+    aws_lambda.flows = flows;
+    aws_topic.flows = flows;
+	
+flows[0].func(input);
 };
