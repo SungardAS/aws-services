cef6c750daca64ef7f91094b1ece6f163a3323db
bhargavapratik <pratik.bhargava@sungardas.com>
2016-08-31 19:42:11 +0530

    MA-15: Modified code,removed unnecessary files.

--- awsconfig-notification-alert/build.f/particles-initial/config/default.js
+++ awsconfig-notification-alert/build.f/particles-initial/config/default.js
@@ -2,15 +2,6 @@ var config = {
   s3: [
     {
       aws: {
-        region: 'us-east-1',
-        bucket: 'sgas.particles-notificationalert.442294194136.us-east-1'
-      },
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
         region: 'us-west-2',
         bucket: 'sgas.particles-notificationalert.442294194136.us-west-2'
       },
