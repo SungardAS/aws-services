aff5f1dbae214812fa226c48e6a25b404f81c3ce
chandra.mishra <chandra.mishra@sungardas.com>
2016-09-20 18:54:01 +0530

    WIP

--- enhancesnapshot/index_enhance_snapshot_launcher.js
+++ enhancesnapshot/index_enhance_snapshot_launcher.js
@@ -2,6 +2,7 @@
 exports.handler = function (event, context) {
 
   var aws_sts = new (require('../lib/aws/sts'))();
+  //var aws_stack = new (require('../lib/aws/stack'))();
   var aws_cfn = new (require('../lib/aws/awscfn.js'))();
   var aws_ec2 = new (require('../lib/aws/ec2.js'))();
   var aws  = require("aws-sdk");
@@ -23,9 +24,10 @@ exports.handler = function (event, context) {
   if (sessionName == null || sessionName == "") {
     sessionName = "session";
   }
-  function succeeded(input) { context.done(null, input.rules);}
+  function succeeded(input) { context.done(input.res,true);}
   function failed(input) { context.done(null, false); }
-  function errored(err) { context.fail(err, null); }
+  function errored(err) { context.fail(err, false); }
+  console.log(event);
   var params = {
        KeyName:event.KeyName,
        NameTag:event.NameTag,
@@ -39,6 +41,7 @@ exports.handler = function (event, context) {
   };
     var input = {
        sessionName: sessionName,
+       stackName: event.stackName,
        roles: roles,
        region: event.region,
        params:params,
@@ -46,12 +49,19 @@ exports.handler = function (event, context) {
        Ami:event.Ami,
        account:event.account
     };
-    var flows = [
-       {func:aws_ec2.enableLaunchAMIPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
-       {func:aws_sts.assumeRoles, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
-       {func:aws_cfn.createESnapShotCFNStack, success:succeeded, failure:failed, error:errored},
-    ];
-    aws_ec2.flows = flows;
+    if(event.type == "createStack"){
+        var flows = [
+           {func:aws_ec2.enableLaunchAMIPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
+           {func:aws_sts.assumeRoles, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
+           {func:aws_cfn.createESnapShotCFNStack, success:succeeded, failure:failed, error:errored},
+        ];
+        aws_ec2.flows = flows;
+    }else if(event.type == "getStackStatus"){
+        var flows = [
+           {func:aws_sts.assumeRoles, success:aws_cfn.getStackStatus, failure:failed, error:errored},
+           {func:aws_cfn.getStackStatus, success:succeeded, failure:failed, error:errored},
+        ];
+    }
     aws_sts.flows = flows;
     aws_cfn.flows = flows;
 
