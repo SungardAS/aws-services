bf4ac72e550b98da02c47b2cd66e9cd3a99ebe63
chandra.mishra <chandra.mishra@sungardas.com>
2016-09-21 19:56:36 +0530

    modified these files for add s3bucket policy

--- enhancesnapshot/index_enhance_snapshot_launcher.js
+++ enhancesnapshot/index_enhance_snapshot_launcher.js
@@ -3,8 +3,9 @@ exports.handler = function (event, context) {
 
   var aws_sts = new (require('../lib/aws/sts'))();
   //var aws_stack = new (require('../lib/aws/stack'))();
-  var aws_cfn = new (require('../lib/aws/awscfn.js'))();
-  var aws_ec2 = new (require('../lib/aws/ec2.js'))();
+  var aws_cfn = new (require('../lib/aws/awscfn'))();
+  var aws_ec2 = new (require('../lib/aws/ec2'))();
+  var aws_s3 = new (require('../lib/aws/s3bucket'))();
   var aws  = require("aws-sdk");
 
   if (!event.federateRoleName)  event.federateRoleName = "federate";
@@ -50,12 +51,18 @@ exports.handler = function (event, context) {
        account:event.account
     };
     if(event.type == "createStack"){
+        var policy = "{\"Id\": \"Policy1474458972817\",\"Version\": \"2012-10-17\",\"Statement\": [{\"Sid\": \""+event.uid+"\",\"Action\": [ \"s3:GetObject\" ],\"Effect\": \"Allow\",\"Resource\": \"arn:aws:s3:::"+event.bucketName+"/*\",\"Principal\": {\"AWS\":\""+event.account+"\"}}]}";
+        input.bucketName = event.bucketName;
+        input.policyDocument = policy;
+     
         var flows = [
+           {func:aws_s3.addPolicy, success:aws_ec2.enableLaunchAMIPermission, failure:failed, error:errored},
            {func:aws_ec2.enableLaunchAMIPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
            {func:aws_sts.assumeRoles, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
            {func:aws_cfn.createESnapShotCFNStack, success:succeeded, failure:failed, error:errored},
         ];
         aws_ec2.flows = flows;
+        aws_s3.flows = flows;
     }else if(event.type == "getStackStatus"){
         var flows = [
            {func:aws_sts.assumeRoles, success:aws_cfn.getStackStatus, failure:failed, error:errored},
