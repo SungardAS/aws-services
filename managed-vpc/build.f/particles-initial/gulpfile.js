de75afff568d713c9a89838778ef78a7c4fd4428
bhargavapratik <pratik.bhargava@sungardas.com>
2016-12-13 13:35:30 +0530

    cleaned folder structure

--- managed-vpc/build.f/particles-initial/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-config = require('config');
-
-// Will add necessary gulp tasks to build, compile and validate
-// CloudFormation templates
-
-require('condensation').buildTasks(gulp,config);
