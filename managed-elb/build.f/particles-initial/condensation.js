0b9be191508aa10887293aca59068f294e227975
Rajkumar Shivage <rajkumar.shivage@sungardas.com>
2017-02-10 02:16:46 -0500

    added managed-elb particle

---
+++ managed-elb/build.f/particles-initial/condensation.js
@@ -0,0 +1,21 @@
+var git = require('gulp-git'),
+gulp = require('gulp'),
+vfs = require('vinyl-fs'),
+zip = require('gulp-zip'),
+exec = require('child_process').exec;
+
+module.exports.initialize = function(cb) {
+  var child = exec('npm install', function(error, stdout, stderr) {
+    if (error) return cb(error);
+  });
+
+  vfs.src(["managed-vpc/index_*.js",  "managed-vpc/json/*.json", "lib/flow_controller.js", "lib/aws/*.js"],{cwd:'../../..', base:'../../..'})
+  .pipe(zip('managed-vpc.zip'))
+  .pipe(gulp.dest('./particles/assets'))
+  .on('end', function(err, data) {
+    vfs.src(['cloudformation/*.js'],{cwd:'../../..', base:'../../..'})
+    .pipe(zip('cloudformation_builder.zip'))
+    .pipe(gulp.dest('./particles/assets'))
+      .on('end', cb);
+  });
+};
