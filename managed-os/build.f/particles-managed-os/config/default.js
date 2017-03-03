95bdb54fbf88c5e19be4d892fcd422c616e82ae6
Rajkumar Shivage <rajkumar.shivage@sungardas.com>
2017-03-03 09:24:26 -0500

    add lambda function name property

--- managed-os/build.f/particles-managed-os/config/default.js
+++ managed-os/build.f/particles-managed-os/config/default.js
@@ -2,35 +2,8 @@ var config = {
   s3: [
     {
       aws: {
-        region: 'us-east-1',
-        bucket: 'sgas.particles-managed-os.442294194136.us-east-1'
-      },
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'us-east-2',
-        bucket: 'sgas.particles-managed-os.442294194136.us-east-2'
-      },
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
         region: 'us-west-1',
         bucket: 'sgas.particles-managed-os.442294194136.us-west-1'
-      },
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'us-west-2',
-        bucket: 'sgas.particles-managed-os.442294194136.us-west-2'
       },
       clean: true,
       validate: true,
