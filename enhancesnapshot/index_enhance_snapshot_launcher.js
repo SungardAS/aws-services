1d93f22502b56f73cfffce3002bba6506f20efc3
chandra.mishra <chandra.mishra@sungardas.com>
2017-08-25 12:27:12 +0000

    refactoring code for MA-252

--- enhancesnapshot/index_enhance_snapshot_launcher.js
+++ enhancesnapshot/index_enhance_snapshot_launcher.js
@@ -1,4 +1,3 @@
-//chandra
 exports.handler = function (event, context) {
 
   var aws_sts = new (require('../lib/aws/sts'))();
@@ -8,19 +7,20 @@ exports.handler = function (event, context) {
   var aws_s3 = new (require('../lib/aws/s3bucket'))();
   var aws  = require("aws-sdk");
 
-  if (!event.federateRoleName)  event.federateRoleName = "federate";
+  //if (!event.federateRoleName)  event.federateRoleName = "federate";
 
-  var roles = [];
-  if (event.federateAccount) {
-    roles.push({roleArn:'arn:aws:iam::' + event.federateAccount + ':role/' + event.federateRoleName});
-    var admin_role = {roleArn:'arn:aws:iam::' + event.account + ':role/' + event.roleName};
-    if (event.roleExternalId) {
-      admin_role.externalId = event.roleExternalId;
-    }
-    roles.push(admin_role);
-  }
-  console.log(roles);
+  //var roles = [];
+  //if (event.federateAccount) {
+  //  roles.push({roleArn:'arn:aws:iam::' + event.federateAccount + ':role/' + event.federateRoleName});
+  //  var admin_role = {roleArn:'arn:aws:iam::' + event.account + ':role/' + event.roleName};
+  //  if (event.roleExternalId) {
+  //    admin_role.externalId = event.roleExternalId;
+  //  }
+  //  roles.push(admin_role);
+ // }
+  //console.log(roles);
 
+  console.log(event);
   var sessionName = event.sessionName;
   if (sessionName == null || sessionName == "") {
     sessionName = "session";
@@ -28,60 +28,66 @@ exports.handler = function (event, context) {
   function succeeded(input) { context.done(input.res,true);}
   function failed(input) { context.done(null, false); }
   function errored(err) { context.fail(err, false); }
-  console.log(event);
   var params = {
-       KeyName:event.KeyName,
-       NameTag:event.NameTag,
-       SourceCIDR:event.SourceCIDR,
-       SshLocation:event.SshLocation,
-       SubnetId:event.SubnetId,
-       VolumeIOps:event.VolumeIOps,
-       VolumeSize:event.VolumeSize,
-       VpcId:event.VpcId,
-       Eip:event.Eip,
-       UUID:event.uuid
+    KeyName:event.KeyName,
+    NameTag:event.NameTag,
+    SourceCIDR:event.SourceCIDR,
+    SshLocation:event.SshLocation,
+    SubnetId:event.SubnetId,
+    VolumeIOps:event.VolumeIOps,
+    VolumeSize:event.VolumeSize,
+    VpcId:event.VpcId,
+    Eip:event.Eip,
+    UUID:event.uuid
   };
-    var input = {
-       sessionName: sessionName,
-       stackName: event.stackName,
-       roles: roles,
-       region: event.region,
-       params:params,
-       s3Url:event.url,
-       Ami:event.Ami,
-       account:event.account
-    };
-    if(event.actionType == "createStack"){
-        var policy = "{\"Id\": \"Policy1474458972817\",\"Version\": \"2012-10-17\",\"Statement\": [{\"Sid\": \""+event.uuid+"\",\"Action\": [ \"s3:GetObject\" ],\"Effect\": \"Allow\",\"Resource\": \"arn:aws:s3:::"+event.bucketName+"/*\",\"Principal\": {\"AWS\":\""+event.account+"\"}}]}";
-        input.bucketName = event.bucketName;
-        input.policyDocument = policy;
-     
-        var flows = [
-           {func:aws_s3.addPolicy, success:aws_ec2.enableLaunchAMIPermission, failure:failed, error:errored},
-           {func:aws_ec2.enableLaunchAMIPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
-           {func:aws_sts.assumeRoles, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
-           {func:aws_cfn.createESnapShotCFNStack, success:succeeded, failure:failed, error:errored},
-        ];
-        aws_ec2.flows = flows;
-        aws_s3.flows = flows;
-    }else if(event.actionType == "deleteStack"){
-        input.bucketName = event.bucketName;
-        var flows = [
-           {func:aws_s3.deletePolicy, success:aws_ec2.deleteLaunchAMIPermission, failure:failed, error:errored},
-           {func:aws_ec2.deleteLaunchAMIPermission, success:aws_sts.assumeRoles, failure:failed, error:errored},
-           {func:aws_sts.assumeRoles, success:aws_cfn.deleteStack, failure:failed, error:errored},
-           {func:aws_cfn.deleteStack, success:succeeded, failure:failed, error:errored},
-        ];
-        aws_ec2.flows = flows;
-        aws_s3.flows = flows;
-    }else if(event.actionType == "getStackStatus"){
-        var flows = [
-           {func:aws_sts.assumeRoles, success:aws_cfn.getStackStatus, failure:failed, error:errored},
-           {func:aws_cfn.getStackStatus, success:succeeded, failure:failed, error:errored},
-        ];
-    }
-    aws_sts.flows = flows;
-    aws_cfn.flows = flows;
 
-    flows[0].func(input);
+  var creds = new aws.Credentials({
+    accessKeyId: event.creds.AccessKeyId,
+    secretAccessKey: event.creds.SecretAccessKey,
+    sessionToken: event.creds.SessionToken
+  });
+    
+  var input = {
+    sessionName: sessionName,
+    stackName: event.stackName,
+    region: event.region,
+    params:params,
+    s3Url:event.url,
+    Ami:event.Ami,
+    account:event.account,
+    creds:creds
+  };
+  if(event.actionType == "createStack"){
+    var policy = "{\"Id\": \"Policy1474458972817\",\"Version\": \"2012-10-17\",\"Statement\": [{\"Sid\": \""+event.uuid+"\",\"Action\": [ \"s3:GetObject\" ],\"Effect\": \"Allow\",\"Resource\": \"arn:aws:s3:::"+event.bucketName+"/*\",\"Principal\": {\"AWS\":\""+event.account+"\"}}]}";
+    input.bucketName = event.bucketName;
+    input.policyDocument = policy;
+    input.selfAccount = true;
+     
+    var flows = [
+      {func:aws_s3.addPolicy, success:aws_ec2.enableLaunchAMIPermission, failure:failed, error:errored},
+      {func:aws_ec2.enableLaunchAMIPermission, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
+      //{func:aws_sts.assumeRoles, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
+      {func:aws_cfn.createESnapShotCFNStack, success:succeeded, failure:failed, error:errored},
+    ];
+    aws_ec2.flows = flows;
+    aws_s3.flows = flows;
+  }else if(event.actionType == "deleteStack"){
+    input.bucketName = event.bucketName;
+    var flows = [
+      {func:aws_s3.deletePolicy, success:aws_ec2.deleteLaunchAMIPermission, failure:failed, error:errored},
+      {func:aws_ec2.deleteLaunchAMIPermission, success:aws_cfn.deleteStack, failure:failed, error:errored},
+     //{func:aws_sts.assumeRoles, success:aws_cfn.deleteStack, failure:failed, error:errored},
+      {func:aws_cfn.deleteStack, success:succeeded, failure:failed, error:errored},
+    ];
+    aws_ec2.flows = flows;
+    aws_s3.flows = flows;
+  }else if(event.actionType == "getStackStatus"){
+    var flows = [
+   //{func:aws_sts.assumeRoles, success:aws_cfn.getStackStatus, failure:failed, error:errored},
+      {func:aws_cfn.getStackStatus, success:succeeded, failure:failed, error:errored},
+    ];
+  }
+  //aws_sts.flows = flows;
+  aws_cfn.flows = flows;
+  flows[0].func(input);
 };
