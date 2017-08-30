f5b653391153a236313d715413be4344afd9015e
chandra.mishra <chandra.mishra@sungardas.com>
2017-08-30 04:09:18 +0000

    WIP

--- enhancesnapshot/index_enhance_snapshot_launcher.js
+++ enhancesnapshot/index_enhance_snapshot_launcher.js
@@ -1,24 +1,9 @@
 exports.handler = function (event, context) {
 
-  //var aws_sts = new (require('../lib/aws/sts'))();
-  //var aws_stack = new (require('../lib/aws/stack'))();
   var aws_cfn = new (require('../lib/aws/awscfn'))();
   var aws_ec2 = new (require('../lib/aws/ec2'))();
   var aws_s3 = new (require('../lib/aws/s3bucket'))();
   var aws  = require("aws-sdk");
-
-  //if (!event.federateRoleName)  event.federateRoleName = "federate";
-
-  //var roles = [];
-  //if (event.federateAccount) {
-  //  roles.push({roleArn:'arn:aws:iam::' + event.federateAccount + ':role/' + event.federateRoleName});
-  //  var admin_role = {roleArn:'arn:aws:iam::' + event.account + ':role/' + event.roleName};
-  //  if (event.roleExternalId) {
-  //    admin_role.externalId = event.roleExternalId;
-  //  }
-  //  roles.push(admin_role);
- // }
-  //console.log(roles);
 
   console.log(event);
   var sessionName = event.sessionName;
@@ -66,7 +51,6 @@ exports.handler = function (event, context) {
     var flows = [
       {func:aws_s3.addPolicy, success:aws_ec2.enableLaunchAMIPermission, failure:failed, error:errored},
       {func:aws_ec2.enableLaunchAMIPermission, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
-      //{func:aws_sts.assumeRoles, success:aws_cfn.createESnapShotCFNStack, failure:failed, error:errored},
       {func:aws_cfn.createESnapShotCFNStack, success:succeeded, failure:failed, error:errored},
     ];
     aws_ec2.flows = flows;
@@ -77,18 +61,15 @@ exports.handler = function (event, context) {
     var flows = [
       {func:aws_s3.deletePolicy, success:aws_ec2.deleteLaunchAMIPermission, failure:failed, error:errored},
       {func:aws_ec2.deleteLaunchAMIPermission, success:aws_cfn.deleteStack, failure:failed, error:errored},
-     //{func:aws_sts.assumeRoles, success:aws_cfn.deleteStack, failure:failed, error:errored},
       {func:aws_cfn.deleteStack, success:succeeded, failure:failed, error:errored},
     ];
     aws_ec2.flows = flows;
     aws_s3.flows = flows;
   }else if(event.actionType == "getStackStatus"){
     var flows = [
-   //{func:aws_sts.assumeRoles, success:aws_cfn.getStackStatus, failure:failed, error:errored},
       {func:aws_cfn.getStackStatus, success:succeeded, failure:failed, error:errored},
     ];
   }
-  //aws_sts.flows = flows;
   aws_cfn.flows = flows;
   flows[0].func(input);
 };
