c6a4b656803581b9a29471ff74360d1e106658a2
chandra.mishra <chandra.mishra@sungardas.com>
2017-10-09 09:40:59 +0000

    resolved bug MA-1955

---
+++ aws-federation/build.f/particles-aws-federation/condensation.js
@@ -0,0 +1,22 @@
+var git = require('gulp-git'),
+gulp = require('gulp'),
+vfs = require('vinyl-fs'),
+zip = require('gulp-zip'),
+exec = require('child_process').exec;
+
+module.exports.initialize = function(cb) {
+  var child = exec('npm install', function(error, stdout, stderr) {
+    if (error) return cb(error);
+  });
+
+  //vfs.src(['awsconfigrules/index_*.js', 'awsconfigrules/json/*.json', 'awsconfigrules/node_modules/**/*', 'lib/flow_controller.js', 'lib/aws/*.js' , 'lib/aws-promise/*.js'],{cwd:'../../..', base:'../../..'})
+  vfs.src(['aws-federation/index.js', 'aws-federation/json/*.json', 'aws-federation/node_modules/**/*', 'lib/flow_controller.js', 'lib/aws/*.js' , 'lib/aws-promise/*.js'],{cwd:'../../..', base:'../../..'})
+  .pipe(zip('aws-federation.zip'))
+  .pipe(gulp.dest('./particles/assets'))
+  .on('end', function(err, data) {
+    vfs.src(['cloudformation/index_lambda_deployer.js', 'cloudformation/lambda_deployer.js', 'cloudformation/index_iam_federation.js', 'cloudformation/iam_federation.js'],{cwd:'../../..', base:'../../..'})
+    .pipe(zip('cloudformation_builder.zip'))
+    .pipe(gulp.dest('./particles/assets'))
+    .on('end', cb);
+  });
+};
---
+++ aws-federation/build.f/particles-aws-federation/config/default.js
@@ -0,0 +1,25 @@
+var config = {
+  s3: [
+    {
+      aws: {
+        region: 'us-east-2',
+        bucket: 'sgas.particles-aws-federation.442294194136.us-east-2'
+      },
+      clean: true,
+      validate: true,
+      create: true
+    },
+    {
+      aws: {
+        region: 'us-east-2',
+        bucket: 'sgas.particles-aws-federation.442294194136.us-east-2'
+      },
+      clean: true,
+      validate: true,
+      create: true
+    }
+  ],
+  dist: 'dist'
+};
+
+module.exports = config;
