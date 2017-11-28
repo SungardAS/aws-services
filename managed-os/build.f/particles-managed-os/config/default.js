35234972f51418df05615552256f3be7cf3cefc8
Rajkumar Shivage <rajkumar.shivage@sungardas.com>
2017-11-28 04:05:10 -0500

    modified bucket name

--- managed-os/build.f/particles-managed-os/config/default.js
+++ managed-os/build.f/particles-managed-os/config/default.js
@@ -3,7 +3,7 @@ var config = {
     {
       aws: {
         region: process.env.AWS_REGION,
-        bucket: `sgas.particles-managed-os-rajkumar-${process.env.ACCOUNT}-${process.env.AWS_REGION}`
+        bucket: `sgas.particles-managed-os-${process.env.ACCOUNT}-${process.env.AWS_REGION}`
       },
       clean: true,
       validate: true,
