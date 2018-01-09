e7444f973a76c628af86fb38d4a813ad0c942116
Raj Shivage <rajkumar.shivage@sungardas.com>
2018-01-09 14:35:49 +0000

    MA-2354 modified awsconfigrules particle to support multiregion deployment

--- awsconfigrules/build.f/particles-awsconfigrules/config/default.js
+++ awsconfigrules/build.f/particles-awsconfigrules/config/default.js
@@ -2,17 +2,8 @@ var config = {
   s3: [
     {
       aws: {
-        region: 'us-east-1',
-        bucket: 'sgas.particles-awsconfig.442294194136.us-east-1'
-      },
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'us-west-2',
-        bucket: 'sgas.particles-awsconfig.442294194136.us-west-2'
+        region: process.env.AWS_REGION,
+        bucket: `sgas.particles-managed-os.${process.env.ACCOUNT}.${process.env.AWS_REGION}`
       },
       clean: true,
       validate: true,
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -o errexit
+#set -o errexit
 set -o nounset
 
 FILENAME='particle-region-mapping'
