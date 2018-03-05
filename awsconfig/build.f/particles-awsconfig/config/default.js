05345d9be19dddd027d79e0ce53927c7df63f396
chandra.mishra <chandra.mishra@sungardas.com>
2018-03-05 11:46:27 +0000

    modified these files for deploy aws-services based on region and account

--- awsconfig/build.f/particles-awsconfig/config/default.js
+++ awsconfig/build.f/particles-awsconfig/config/default.js
@@ -2,17 +2,8 @@ var config = {
   s3: [
     {
       aws: {
-        region: 'us-east-1',
-        bucket: 'sgas.particles-awsconfig.622821376834.us-east-1'
-      },
-      clean: true,
-      validate: true,
-      create: true
-    },
-    {
-      aws: {
-        region: 'us-west-2',
-        bucket: 'sgas.particles-awsconfig.622821376834.us-west-2'
+        region: process.env.AWS_REGION,
+        bucket: `sgas.particles-awsconfig.${process.env.ACCOUNT}.${process.env.AWS_REGION}`
       },
       clean: true,
       validate: true,
